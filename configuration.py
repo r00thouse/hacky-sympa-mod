f96c55ba1ba3515ad5d432d34bcc5e9e8cc8b3fe
Sergio Guillen <serguimant@gmail.com>
2016-01-06 22:54:04 -0400

    Refacto to accept more settings

--- configuration.py
+++ configuration.py
@@ -4,18 +4,17 @@ import json
 settings = {
     'subscribersFile': './subscribers.json',
     'logFile': './hackymod.log',
+    'simpaEmail': None,
+    'imapSSLServer': None,
+    'imapSSLPort': None,
     'moderatorEmail': None,
     'moderatorPassword': None,
 }
 
 def loadConfiguration(filename):
     content = getFileContent(filename, parseJson=True)
-    if content.has_key('subscribersFile'):
-        settings['subscribersFile'] = content['subscribersFile']
-    if content.has_key('logFile'):
-        settings['logFile'] = content['logFile']
-    if content.has_key('moderatorEmail'):
-        settings['moderatorEmail'] = content['moderatorEmail']
-    if content.has_key('moderatorPassword'):
-        settings['moderatorPassword'] = content['moderatorPassword']
+    settingsKeys = settings.keys()
 
+    for key in settingsKeys:
+        if content.has_key(key):
+            settings[key] = content[key]
